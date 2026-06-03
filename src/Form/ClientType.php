55f73d724d0d654561334cfbceb8b3bcab66bfbd
Tomtom100290 <tom.n974@yahoo.com>
2026-06-03 16:23:54 +0400

    Mise en place du CRUD Client

---
+++ src/Form/ClientType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('raisonSocial')
+            ->add('adresse')
+            ->add('ville')
+            ->add('codePostal')
+            ->add('numTel')
+            ->add('email')
+            ->add('positionX')
+            ->add('positionY')
+            ->add('dateCreation', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('topActif')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
